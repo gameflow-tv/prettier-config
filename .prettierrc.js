079b322e9987b8e0191906233dbd92a12d531713
Arne Molland <arne@molland.sh>
2022-06-11 10:59:17 +0200

    package: export JS config

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: false,
+  tabs: false,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSameLine: true,
+  printWidth: 100,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+}
